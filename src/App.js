749f1c7172302f7792d8ee7e09779b9f758c2021
unknown <achyuthareddyborampeta@gmail.com>
2025-01-10 11:01:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -32,10 +32,9 @@ function App() {
     <Router>
       <Header/>
       <Navbar/>
-      <Home/>
       <Routes>
         {/* navbar */}
-            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/" element={<Home />} />
 
             {/* teams */}
             <Route path="/teams/hope" element={<Hope />}/>
